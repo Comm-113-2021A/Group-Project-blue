7cd85b82f1901397e4ed59182ae638f0790b0667
Jason Yan <jasyan@seas.upenn.edu>
2021-04-22 19:59:36 +0000

    Join demographic/vaccine df. Tidy df.

--- data/combine_data.r
+++ data/combine_data.r
@@ -27,10 +27,43 @@ pfizer <- read_csv("data/pfizer.csv") %>%
 janssen_second_dose <- janssen %>%
   mutate(second_dose = 0)
 
+janssen_second_dose <- janssen_second_dose %>%
+  mutate(vaccine = "janssen")
+
+pfizer <- pfizer %>%
+  mutate(vaccine = "pfizer")
+
+moderna <- moderna %>%
+  mutate(vaccine = "moderna")
+
 vaccines <- rbind(janssen_second_dose, moderna, pfizer)
 
 vaccines <- vaccines %>%
   mutate(week = as.Date(week, "%m/%d/%y"))
 
 write_csv(vaccines, "data/vaccines.csv")
+
+
+
+# ----------- demographic -------------------------
+
+demographic <- read_csv("data/demographic.csv")
+
+glimpse(demographic)
+
+#-------------join df --------------
+library(janitor)
+demographic <- demographic %>%
+  select(Location, White, Black, Asian, Hispanic) %>%
+  janitor::clean_names()
+
+vaccines <- vaccines %>%
+  rename(location = "jurisdiction")
+vaccine_demographic <- left_join(vaccines, demographic, by = "location") %>%
+  na.omit()
+
+vaccine_demographic$asian <- lapply(vaccine_demographic$asian, function(x) as.numeric(as.character(x)))
+
+vaccine_demographic <- vaccine_demographic %>%
+  mutate(asian = as.numeric(asian))
 
